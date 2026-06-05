1393b541c52f285035d1d507882f1090aad996e3
AftabAhmed-max <siddiquiaftab2001@gmail.com>
2026-06-05 07:58:53 +0530

    images change

--- src/components/sections/services/ServicesShowcase.tsx
+++ src/components/sections/services/ServicesShowcase.tsx
@@ -1008,6 +1008,7 @@ function PeopleDeskCard() {
         <Image
           src="/images/projects/peopledesk.png"
           alt="PeopleDesk"
+          loading="eager"
           fill
           sizes="(max-width: 900px) 100vw, 33vw"
           style={{
